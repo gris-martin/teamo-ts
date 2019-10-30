ea6556c5626d1382ba4364ddb4acb62a7ae86263
Martin Törnqvist <gris_martin@hotmail.com>
2019-10-30 22:01:58 +0100

    CI

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,8 @@ npm install
 npm run build
 
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dst $TRAVIS_BUILD_DIR/node_modules Dockerfile root@108.61.198.106:~/bots/teamo-master
+
+ssh root@108.61.198.106 "cd ~/bots/$TRAVIS_BRANCH ; docker build -t $TRAVIS_BRANCH . ; docker stop $TRAVIS_BRANCH-instance || true && docker rm $TRAVIS_BRANCH-instance || true"
+ssh root@108.61.198.106 "cd ~/bots/$TRAVIS_BRANCH ; docker stop $TRAVIS_BRANCH-instance || true"
+ssh root@108.61.198.106 "cd ~/bots/$TRAVIS_BRANCH ; docker rm $TRAVIS_BRANCH-instance || true"
+ssh root@108.61.198.106 "cd ~/bots/$TRAVIS_BRANCH ; docker run -e BOT_TOKEN=$BOT_TOKEN --name $TRAVIS_BRANCH-instance -d $TRAVIS_BRANCH"

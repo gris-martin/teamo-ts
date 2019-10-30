14982f9fc409b725a82c7552bb5963c580c4e743
Martin Törnqvist <gris_martin@hotmail.com>
2019-10-30 21:58:44 +0100

    CI

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 # 1. Name of Docker image to build
 # 2. API token for Discord bot
 npm install
-npm build
+npm run build
 
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dst $TRAVIS_BUILD_DIR/node_modules Dockerfile root@108.61.198.106:~/bots/teamo-master

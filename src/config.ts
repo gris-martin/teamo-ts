4a0bfd51046d528248620738d1aa2f679e576bc6
Martin Törnqvist <gris_martin@hotmail.com>
2019-11-30 15:43:03 +0100

    Remove unused config parameters

--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,11 @@
 type Config = {
     token: string,
     prefix: string,
-    commands: string[],
     language: "swedish" | "english";
 }
 
 export const config: Config = {
     "token": `${process.env.BOT_TOKEN}`,
     "prefix": "!",
-    "commands": [
-        "help",
-        "play",
-        "getName"
-    ],
     "language": "swedish"
 }

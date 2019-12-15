058ac0c8096206014fbbaec426b5e51c2a260b65
Martin Törnqvist <gris_martin@hotmail.com>
2019-12-15 13:37:17 +0100

    Remove commented code

--- src/PurgeCommand.ts
+++ src/PurgeCommand.ts
@@ -3,7 +3,6 @@ import { handleWelcomeCommand } from './WelcomeCommand';
 
 export async function handlePurgeCommand(channel: Discord.TextChannel) {
     const messages = await channel.messages.fetch();
-    // channel.awaitMessages(() => true, {time: 1}).then(collected => console.log("Collected", collected));
 
     const welcomeMessage = messages.last();
     console.log(welcomeMessage);

55255c10accf9104fe5a05ec82dd9c7b1b81a00a
Martin Törnqvist <gris_martin@hotmail.com>
2019-12-15 19:44:45 +0100

    Revert "Revert "Merge branch 'master' into deploy""
    This reverts commit fa541400d69b41e5c312f649e71451cbdf1ce8d8.

--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,13 @@
 type Config = {
     token: string,
-    prefix: string,
     language: "swedish" | "english",
     updateInterval: number,
-    useSpecificChannel: boolean,
     channelID: string
 }
 
 export const config: Config = {
     "token": process.env.TEAMO_BOT_TOKEN,
-    "prefix": "!",
     "language": "swedish",
     "updateInterval": 15,
-    "useSpecificChannel": true,
     "channelID": process.env.TEAMO_CHANNEL_ID || "-1"
 }

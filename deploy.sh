52bdad3c34d0d192b48923a018b9b7fd724c9003
Martin Törnqvist <gris_martin@hotmail.com>
2019-12-21 14:53:59 +0100

    CI: Test deploy

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,12 @@
 # 2. API token for Discord bot
 set -x
 
-branch=$(echo $TRAVIS_BRANCH | sed -e 's/\//-/g')
+# npm install
+# npm run build
 
-npm install
-npm run build
+rsync -r --delete-after --quiet $GITHUB_WORKSPACE/dst $GITHUB_WORKSPACE/node_modules Dockerfile root@108.61.198.106:~/bots/$branch
 
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dst $TRAVIS_BUILD_DIR/node_modules Dockerfile root@108.61.198.106:~/bots/$branch
-
-ssh root@108.61.198.106 "cd ~/bots/$branch ; docker build -t $branch ."
-ssh root@108.61.198.106 "cd ~/bots/$branch ; docker stop $branch-instance || true"
-ssh root@108.61.198.106 "cd ~/bots/$branch ; docker rm $branch-instance || true"
-ssh root@108.61.198.106 "cd ~/bots/$branch ; docker run -e TEAMO_BOT_TOKEN=$TEAMO_BOT_TOKEN -e TEAMO_CHANNEL_ID=$TEAMO_CHANNEL_ID -e TZ=Europe/Stockholm --restart unless-stopped --name $branch-instance -d $branch"
+ssh root@108.61.198.106 "cd ~/bots/$DOCKER_NAME ; docker build -t $branch ."
+ssh root@108.61.198.106 "cd ~/bots/$DOCKER_NAME ; docker stop $branch-instance || true"
+ssh root@108.61.198.106 "cd ~/bots/$DOCKER_NAME ; docker rm $branch-instance || true"
+ssh root@108.61.198.106 "cd ~/bots/$DOCKER_NAME ; docker run -e TEAMO_BOT_TOKEN=$TEAMO_BOT_TOKEN -e TEAMO_CHANNEL_ID=$TEAMO_CHANNEL_ID -e TZ=Europe/Stockholm --restart unless-stopped --name $DOCKER_NAME-instance -d $DOCKER_NAME"
